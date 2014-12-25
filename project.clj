15709458be86e73fc7fbd8398280852593cb6ecb
Lauri Hahne <lauri.hahne@leonidasoy.fi>
2014-12-25 14:18:36 +0200

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject m-xhrio "1.0.0"
+(defproject m-xhrio "1.0.1"
   :description "A reasonable ClojureScript interface for goog.net.XhrIo"
   :license "Eclipse Public License 1.0"
   :url "https://github.com/lhahne/m-xhrio")
