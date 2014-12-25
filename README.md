5605acc29ada51a251e6e064310df778f0a27bca
Lauri Hahne <lauri.hahne@leonidasoy.fi>
2014-12-25 16:44:57 +0200

    add support for all xhrio params, bump version

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ A jayq let-ajax style ClojureScript wrapper for goog.net.XhrIo.
 ## Usage
 Add it in leiningen
 
-    [m-xhrio "1.0.0"]
+    [m-xhrio "1.1.0"]
 
 Use it
 
@@ -14,5 +14,11 @@ Use it
       (:require-macros [m-xhrio.macros :refer [let-xhr]])
       (:require [m-xhrio.xhr]))
   
-    (let-xhr [a {:url "http://localhost:3000/" :method "GET"}]
+    (let-xhr [a {:url "http://localhost:3000/" :method :get}]
       (.log js/console (.getResponseText a)))
+
+## Changes
+
+### 1.1
+- Add support for all the parameters of XhrIo
+- Method as a keyword
--- src/m_xhrio/xhr.cljs
+++ src/m_xhrio/xhr.cljs
@@ -1,8 +1,11 @@
 (ns m-xhrio.xhr
-  (:require [goog.net.XhrIo :as xhr]))
+  (:require [goog.net.XhrIo :as xhr]
+            [clojure.string :as string]))
 
 (def xhr-m
   {:return identity
-   :bind (fn [{:keys [:url :method]} f]
-           (xhr/send url (fn [e] (f (.-target e))) method))
+   :bind (fn [{:keys [:url :method :content :headers :timeoutInterval :withCredentials]} f]
+             (let [cb (fn [e] (f (.-target e)))
+                   method (-> method name string/upper-case)]
+                  (xhr/send url cb method content headers timeoutInterval withCredentials)))
    :zero identity})
